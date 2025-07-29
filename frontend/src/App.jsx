374b612d72408834a00573d8324065f28f6ea464
Johanne Alix Motsem <yemgnialix@gmail.com>
2025-07-29 22:02:15 +0200

    Revert "Merge Backend & Updated FrontEnd"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 // App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UserProfile from './components/UserProfile';
 import Login from './components/Login';
 import AlertsDashboard from './components/AlertsDashboard';
 import DiagnosticTests from './components/DiagnosticTests';
 import './global.css';
 
-  function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+    <Route path="/" element={<Login/>}/>
         <Route path="/login" element={<Login/>} />
         <Route path="/profile" element={<UserProfile />} />
         <Route path="/alerts" element={<AlertsDashboard />} />
@@ -20,5 +20,3 @@ import './global.css';
     </Router>
   );
 }
-
-export default App;
